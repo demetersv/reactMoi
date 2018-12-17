9e650d9935e4a47ba95a273f6ed98793b79b288e
cwilson-demeter <cwilson@demetersv.com>
2018-12-17 07:58:17 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Clock from './Clock';
+import MofoButton from './MofoButton';
 class App extends Component {
+ 
+ constructor(props) {
+    super(props);
+	 // ici on defini le this de la fonction, il correspondera au this du compoenent APP
+	 
+	  this.changeColor = this.changeColor.bind(this);
+	  
+	  
+	  
+	  
+	  
+    this.state = {
+      bg: 'yellow'
+    };
+  }
+  
+  
+  changeColor(fromChild){
+		this.changeColor = this.changeColor.bind(fromChild);
+		this.setState({
+			bg: fromChild,
+		
+		});
+  }
+	
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
+		   <p className="App-intro">
+        </p>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h2>Welcome to React</h2>
+			    <Clock ></Clock>
+				 <div style={{height: '200px', width: '200px', backgroundColor: this.state.bg}}>
+					 <MofoButton actionSpecifiqueAttribuerAucomponent={this.changeColor} color={'pink'}></MofoButton>
+					 <MofoButton actionSpecifiqueAttribuerAucomponent={this.changeColor} ></MofoButton>
+			
+				</div>
+        </div>
+       
+		       
+
       </div>
     );
   }
